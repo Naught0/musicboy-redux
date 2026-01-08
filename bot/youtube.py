3c2e8f0351594975ec48b42f6576e877bdf6c03a
James E <4589250+Naught0@users.noreply.github.com>
2026-01-07 23:52:53 -0500

    Add convenience methods, improve DTOs

--- bot/youtube.py
+++ bot/youtube.py
@@ -1,4 +1,7 @@
 import asyncio
+from collections.abc import Mapping
+from typing import Any, TypedDict
+from urllib import parse
 
 import discord
 import yt_dlp
@@ -33,8 +36,8 @@ async def get_playlist_urls(url: str, *, loop: asyncio.AbstractEventLoop | None 
     meta_opts = YTDL_OPTIONS.copy()
     meta_opts["extract_flat"] = True
 
-    with yt_dlp.YoutubeDL(meta_opts) as ydl:
-        data = await loop.run_in_executor(
+    with yt_dlp.YoutubeDL(meta_opts) as ydl:  # type: ignore
+        data: Mapping[str, Any] = await loop.run_in_executor(
             None, lambda: ydl.extract_info(url, download=False)
         )
 
@@ -52,33 +55,49 @@ async def get_playlist_urls(url: str, *, loop: asyncio.AbstractEventLoop | None 
     return [url]  # Return as single-item list if it's just one video
 
 
-class YTDLSource(discord.PCMVolumeTransformer):
+class VideoInfo(TypedDict):
+    title: str
+    audio_url: str
+    url: str
 
-    def __init__(self, source, *, data, volume=0.5):
+
+def parse_yt_url(url: str):
+    res = parse.urlparse(url.strip("<>"))
+    qs = parse.parse_qs(res.query)
+    try:
+        playlist_id = qs["list"][0]
+        return f"https://www.youtube.com/playlist?list={playlist_id}"
+    except (KeyError, IndexError):
+        v = qs["v"][0]
+        return f"https://www.youtube.com/watch?v={v}"
+
+
+def get_video_info(url: str):
+    with yt_dlp.YoutubeDL(YTDL_OPTIONS) as ydl:  # type: ignore
+        data = ydl.extract_info(url, download=False)
+
+    if not data:
+        raise SongNotFound(f"Couldn't find data for {url}")
+    if not (title := data.get("title")):
+        raise SongNotFound(f"Couldn't find title for {url}")
+    if not (ytdl_url := data.get("url")):
+        raise SongNotFound(f"Couldn't find url for {url}")
+
+    return VideoInfo(title=title, url=parse_yt_url(url), audio_url=ytdl_url)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source: discord.AudioSource, *, info: VideoInfo, volume=0.5):
         super().__init__(source, volume)
-        self.data = data
-        self.title = data.get("title")
-        self.url = data.get("url")
+        self.title = info["title"]
+        self.url = info["url"]
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=True):
-        loop = loop or asyncio.get_event_loop()
-        with yt_dlp.YoutubeDL(YTDL_OPTIONS) as ydl:
-            data = await loop.run_in_executor(
-                None, lambda: ydl.extract_info(url, download=not stream)
-            )
-
-        if data is None:
-            raise SongNotFound(f"Couldn't find data for {url}")
-
-        if "entries" in data:
-            data = data["entries"][0]
-
-        filename = data["url"] if stream else ydl.prepare_filename(data)
+    def from_video_info(cls, info: VideoInfo):
         return cls(
             discord.FFmpegPCMAudio(
-                filename,
+                info["audio_url"],
                 options=FFMPEG_OPTIONS["options"],
             ),
-            data=data,
+            info=info,
         )
